9fc1bd1a3080bc8660f77ac5406b79cf5b19c596
coinop-logan <coinop.logan@gmail.com>
2020-11-03 13:13:40 +0000

    Wrappers for Staking

---
+++ elm/Contracts/Staking.elm
@@ -0,0 +1,66 @@
+module Contracts.Staking exposing (..)
+
+import Config
+import Contracts.Generated.ERC20 as ERC20
+import Contracts.Generated.StakingRewards as StakingContract
+import Eth
+import Eth.Types exposing (..)
+import Helpers.Eth as EthHelpers
+import Http
+import Task
+import TokenValue exposing (TokenValue)
+
+
+callStake : TokenValue -> (Result Http.Error () -> msg) -> Cmd msg
+callStake amount msgConstructor =
+    Eth.call
+        Config.httpProviderUrl
+        (StakingContract.stake
+            Config.stakingContractAddress
+            (TokenValue.getEvmValue amount)
+        )
+        |> Task.attempt msgConstructor
+
+
+callGetReward : (Result Http.Error () -> msg) -> Cmd msg
+callGetReward msgConstructor =
+    Eth.call
+        Config.httpProviderUrl
+        (StakingContract.getReward
+            Config.stakingContractAddress
+        )
+        |> Task.attempt msgConstructor
+
+
+callExit : (Result Http.Error () -> msg) -> Cmd msg
+callExit msgConstructor =
+    Eth.call
+        Config.httpProviderUrl
+        (StakingContract.exit
+            Config.stakingContractAddress
+        )
+        |> Task.attempt msgConstructor
+
+
+getAmountEarned : Address -> (Result Http.Error TokenValue -> msg) -> Cmd msg
+getAmountEarned address msgConstructor =
+    Eth.call
+        Config.httpProviderUrl
+        (StakingContract.earned
+            Config.stakingContractAddress
+            address
+        )
+        |> Task.attempt
+            (Result.map TokenValue.tokenValue >> msgConstructor)
+
+
+getBalance : Address -> (Result Http.Error TokenValue -> msg) -> Cmd msg
+getBalance address msgConstructor =
+    Eth.call
+        Config.httpProviderUrl
+        (ERC20.balanceOf
+            Config.stakingContractAddress
+            address
+        )
+        |> Task.attempt
+            (Result.map TokenValue.tokenValue >> msgConstructor)
