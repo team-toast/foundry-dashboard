d2f6a9e534a6219007fdf02060efe8039ab9716d
h_crypto_z <h_crypto_z@protonmail.com>
2021-01-16 12:27:43 +0200

    remove action defined elsewhere

--- elm/Contracts/DEthWrapper.elm
+++ elm/Contracts/DEthWrapper.elm
@@ -3,17 +3,15 @@ module Contracts.DEthWrapper exposing (..)
 import Config
 import Contracts.Generated.DEth as Death
 import Eth.Types exposing (Address)
+import Http
+import Task
 import TokenValue exposing (TokenValue)
 
 
-getBalance : Address -> TokenValue
-getBalance address =
-    Death.balanceOf
-        Config.derivedEthContractAddress
-        address
-
-
-getRedemptionValue : Address -> TokenValue -> TokenValue
+getRedemptionValue :
+    Address
+    -> TokenValue
+    -> TokenValue
 getRedemptionValue address dEthBalance =
     let
         nrTokens =
