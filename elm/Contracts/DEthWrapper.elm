4eb79333ee8e1e390dcd6a0aeb7a4ae1d030538e
h_crypto_z <h_crypto_z@protonmail.com>
2021-01-19 11:05:40 +0200

    Build out deth contrac wrapper methods

--- elm/Contracts/DEthWrapper.elm
+++ elm/Contracts/DEthWrapper.elm
@@ -1,27 +1,54 @@
 module Contracts.DEthWrapper exposing (..)
 
-import Config
+import Config exposing (derivedEthContractAddress)
 import Contracts.Generated.DEth as Death
-import Eth.Types exposing (Address)
+import Contracts.Generated.ERC20 as ERC20
+import Eth
+import Eth.Types as Eth exposing (Address)
 import Http
 import Task
 import TokenValue exposing (TokenValue)
 
 
-getRedemptionValue :
-    Address
-    -> TokenValue
-    -> TokenValue
-getRedemptionValue address dEthBalance =
-    let
-        nrTokens =
-            case dEthBalance of
-                Just val ->
-                    val
+deposit : Address -> TokenValue -> Eth.Send
+deposit sender amount =
+    Death.squanderMyEthForWorthlessBeans
+        derivedEthContractAddress
+        sender
+        (TokenValue.getEvmValue amount |> Just)
+        |> Eth.toSend
 
-                Nothing ->
-                    0
-    in
-    Death.calculateRedemptionValue
-        address
-        nrTokens
+
+redeem : Address -> TokenValue -> Eth.Send
+redeem receiver amount =
+    Death.redeem
+        derivedEthContractAddress
+        (TokenValue.getEvmValue amount)
+        receiver
+        |> Eth.toSend
+
+
+getRedeemable : TokenValue -> (Result Http.Error ( TokenValue, TokenValue, TokenValue ) -> msg) -> Cmd msg
+getRedeemable amount msgConstructor =
+    Eth.call
+        Config.httpProviderUrl
+        (Death.calculateRedemptionValue
+            derivedEthContractAddress
+            (TokenValue.getEvmValue amount)
+        )
+        |> Task.map unpackCalculatedRedemptionValue
+        |> Task.attempt msgConstructor
+
+
+unpackCalculatedRedemptionValue : Death.CalculateRedemptionValue -> ( TokenValue, TokenValue, TokenValue )
+unpackCalculatedRedemptionValue data =
+    ( TokenValue.tokenValue data.totalCollateralRedeemed, TokenValue.tokenValue data.fee, TokenValue.tokenValue data.collateralReturned )
+
+
+approveDEthToken : Eth.Send
+approveDEthToken =
+    ERC20.approve
+        Config.derivedEthContractAddress
+        Config.derivedEthContractAddress
+        (TokenValue.maxTokenValue |> TokenValue.getEvmValue)
+        |> Eth.toSend
