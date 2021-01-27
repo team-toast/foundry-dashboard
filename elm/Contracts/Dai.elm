2f8e16a05a39625c3eaeec53d6e579cfcfd47ded
h_crypto_z <h_crypto_z@protonmail.com>
2021-01-27 13:44:35 +0200

    remove unused code with reference to smokesignal

--- elm/Contracts/Dai.elm
+++ elm/Contracts/Dai.elm
@@ -19,23 +19,3 @@ getBalanceCmd owner msgConstructor =
         )
         |> Task.attempt
             (Result.map TokenValue.tokenValue >> msgConstructor)
-
-
-
--- getAllowanceCmd : Address -> (Result Http.Error TokenValue -> msg) -> Cmd msg
--- getAllowanceCmd owner msgConstructor =
---     Eth.call
---         Config.httpProviderUrl
---         (ERC20.allowance
---             Config.daiContractAddress
---             owner
---             Config.smokesignalContractAddress
---         )
---         |> Task.attempt
---             (Result.map TokenValue.tokenValue >> msgConstructor)
--- unlockDaiCall : Call Bool
--- unlockDaiCall =
---     ERC20.approve
---         Config.daiContractAddress
---         Config.smokesignalContractAddress
---         EthHelpers.maxUintValue
