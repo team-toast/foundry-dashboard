c50257c59437b7caed8703f9b0b2bc38d991cdc5
h-crypto-z <h_crypto_z@protonmail.com>
2021-04-05 12:07:21 +0200

    add ports and rearrange file

--- elm/Ports.elm
+++ elm/Ports.elm
@@ -3,7 +3,14 @@ port module Ports exposing (..)
 import Json.Decode exposing (Value)
 
 
-port log : String -> Cmd msg
+
+-- OUT
+
+
+port beginLocationCheck : () -> Cmd msg
+
+
+port bscImport : () -> Cmd msg
 
 
 port connectToWeb3 : () -> Cmd msg
@@ -12,40 +19,47 @@ port connectToWeb3 : () -> Cmd msg
 port consentToCookies : () -> Cmd msg
 
 
-port walletSentryPort : (Json.Decode.Value -> msg) -> Sub msg
+port log : String -> Cmd msg
 
 
-port bscImport : () -> Cmd msg
+port refreshWallet : String -> Cmd msg
 
 
 port txOut : Json.Decode.Value -> Cmd msg
 
 
-port txIn : (Json.Decode.Value -> msg) -> Sub msg
-
-
-port beginLocationCheck : () -> Cmd msg
-
-
-port locationCheckResult : (Json.Decode.Value -> msg) -> Sub msg
+port txSend : Value -> Cmd msg
 
 
 port web3Sign : Json.Decode.Value -> Cmd msg
 
 
-port web3SignResult : (Json.Decode.Value -> msg) -> Sub msg
-
-
 port web3ValidateSig : Json.Decode.Value -> Cmd msg
 
 
-port web3ValidateSigResult : (Json.Decode.Value -> msg) -> Sub msg
+
+-- IN
 
 
 port chainSwitchResponse : (Value -> msg) -> Sub msg
 
 
+port locationCheckResult : (Json.Decode.Value -> msg) -> Sub msg
+
+
+port txIn : (Json.Decode.Value -> msg) -> Sub msg
+
+
+port txSendResponse : (Value -> msg) -> Sub msg
+
+
 port walletResponse : (Value -> msg) -> Sub msg
 
 
-port refreshWallet : String -> Cmd msg
+port walletSentryPort : (Json.Decode.Value -> msg) -> Sub msg
+
+
+port web3SignResult : (Json.Decode.Value -> msg) -> Sub msg
+
+
+port web3ValidateSigResult : (Json.Decode.Value -> msg) -> Sub msg
